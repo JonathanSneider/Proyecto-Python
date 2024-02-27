0b4b66663a7b5f165e66f6df34b8f2791b5bc307
JonathanSneider <158193131+JonathanSneider@users.noreply.github.com>
2024-02-27 13:53:41 -0500

    Menus Terminados

--- main.py
+++ main.py
@@ -1,8 +1,113 @@
 import os
 import json
-
-
+import modulos.menus as mn
+import sys
 if __name__ == "__main__":
     Runpro = True
     while Runpro:
-        
+        op = mn.menuPrincipal()
+        if op == "1":
+            Run1 = True
+            while Run1:
+                op1 = mn.menuAct()
+                if op1 == "1":
+                    pass
+                if op1 == "2":
+                    pass
+                if op1 == "3":
+                    pass
+                if op1 == "4":
+                    pass
+                if op1 == "5":
+                    rta1 = 'x'
+                    while (rta1 not in ['N','n','']):
+                        rta1 = input('Desea Regresar al Menu principal No(n/N) Enter(si) Regresar :')
+                        Run1 = bool(rta1)    
+        if op == "2":
+            Run2 = True
+            while Run2:    
+                op2 = mn.menuPer()
+                if op2 == "1":
+                    pass
+                if op2 == "2":
+                    pass
+                if op2 == "3":
+                    pass
+                if op2 == "4":
+                    pass
+                if op2 == "5":
+                    rta2 = 'x'
+                    while (rta2 not in ['N','n','']):
+                        rta2 = input('Desea Regresar al Menu principal No(n/N) Enter(si) Regresar :')
+                        Run2 = bool(rta2)
+        if op == "3":
+            Run3 = True
+            while Run3:  
+                op3 = mn.menuZon()
+                if op3 == "1":
+                    pass
+                if op3 == "2":
+                    pass
+                if op3 == "3":
+                    pass
+                if op3 == "4":
+                    pass
+                if op3 == "5":
+                    rta3 = 'x'
+                    while (rta3 not in ['N','n','']):
+                        rta3 = input('Desea Regresar al Menu principal No(n/N) Enter(si) Regresar :')
+                        Run3 = bool(rta3)
+        if op == "4":
+            Run4 = True
+            while Run4:         
+                op4 = mn.menuAsignacionA()
+                if op4 == "1":
+                    pass
+                if op4 == "2":
+                    pass
+                if op4 == "3":
+                    rta4 = 'x'
+                    while (rta4 not in ['N','n','']):
+                        rta4 = input('Desea Regresar al Menu principal No(n/N) Enter(si) Regresar :')
+                        Run4 = bool(rta4)
+        if op == "5":
+            Run5 = True
+            while Run5:
+                op5 = mn.menuReportes()
+                if op5 == "1":
+                    pass
+                if op5 == "2":
+                    pass
+                if op5 == "3":
+                    pass
+                if op5 == "4":
+                    pass
+                if op5 == "5":
+                    pass
+                if op5 == "6":
+                    rta5 = 'x'
+                    while (rta5 not in ['N','n','']):
+                        rta5 = input('Desea Regresar al Menu principal No(n/N) Enter(si) Regresar :')
+                        Run5 = bool(rta5)
+        if op == "6":
+            Run6 = True
+            while Run6:
+                op6 = mn.menuMAct()
+                if op6 == "1":
+                    pass
+                if op6 == "2":
+                    pass
+                if op6 == "3":
+                    pass
+                if op6 == "4":
+                    pass
+                if op6 == "5":
+                    rta6 = 'x'
+                    while (rta6 not in ['N','n','']):
+                        rta6 = input('Desea Regresar al Menu principal No(n/N) Enter(si) Regresar :')
+                        Run6 = bool(rta6)
+        if op == "7":
+            rta = 'x'
+            while (rta not in ['N','n','']):
+                rta = input('Desea salir No(n/N) Enter(si) Salir :')
+                Runpro = bool(rta)
--- modulos/menus.py
+++ modulos/menus.py
@@ -1,14 +1,139 @@
 import os
 from tabulate import tabulate
 def menuPrincipal():
+    os.system('cls')
+    titulo = """
+    -----------------------------------------
+    * SISTEMA G&C DE INVENTARIO CAMPUSLANDS *
+    -----------------------------------------
+    """
+    print(titulo)
     opciones = ["1","2","3","4","5","6","7"]
-    menu =[["1.", "ACTIVOS"], ["2.", "PERSONAL"], ["3.", "ZONAS"], ["4.", "ASIGNACION DE ACTIVOS"], ["5.", "REPORTES"], ["7.", "MOVIMIENTO DE ACTIVOS"], ["4.", "SALIR"]]
+    menu =[["1.", "ACTIVOS"], ["2.", "PERSONAL"], ["3.", "ZONAS"], ["4.", "ASIGNACION DE ACTIVOS"], ["5.", "REPORTES"], ["6.", "MOVIMIENTO DE ACTIVOS"], ["7.", "SALIR"]]
     print(tabulate(menu, tablefmt="grid"))
     opcion = input("Seleccione una opcion : ")
     if opcion not in opciones:
         print('Ingresaste una opciones invalida')
         os.system('pause')
+        os.system('cls')
         menuPrincipal()
     else:
         return opcion
     
+    
+def menuAct():
+    os.system('cls')
+    titulo = """
+        //////////////////
+        -  MENU ACTIVOS  -
+        //////////////////
+        """
+    print(titulo)
+    opciones = ["1","2","3","4","5"]
+    menu =[["1.", "AGREGAR ACTIVO"], ["2.", "EDITAR ACTIVO"], ["3.", "ELIMINAR ACTIVO"], ["4.", "BUSCAR ACTIVO"], ["5.", "REGRESAR AL MENU PRINCIPAL"]]
+    print(tabulate(menu, tablefmt="grid"))
+    opcion = input("Seleccione una opcion : ")
+    if opcion not in opciones:
+        print('Ingresaste una opciones invalida')
+        os.system('pause')
+        menuAct()
+    else:
+        return opcion
+def menuPer():
+    os.system('cls')
+    titulo2 = """
+        //////////////////
+        - MENU PERSONAL -
+        //////////////////
+    """
+    print(titulo2)
+    opciones = ["1","2","3","4","5"]
+    menu =[["1.", "AGREGAR PERSONAL"], ["2.", "EDITAR PERSONAL"], ["3.", "ELIMINAR PERSONAL"], ["4.", "BUSCAR PERSONAL"], ["5.", "REGRESAR AL MENU PRINCIPAL"]]
+    print(tabulate(menu, tablefmt="grid"))
+    opcion = input("Seleccione una opcion : ")
+    if opcion not in opciones:
+        print('Ingresaste una opciones invalida')
+        os.system('pause')
+        menuPer()
+    else:
+        return opcion
+def menuZon():
+    os.system('cls')
+    titulo3 = """
+        //////////////////
+        -   MENU ZONAS   -
+        //////////////////
+        """
+    print(titulo3)
+    opciones = ["1","2","3","4","5"]
+    menu =[["1.", "AGREGAR ZONA"], ["2.", "EDITAR ZONA"], ["3.", "ELIMINAR ZONA"], ["4.", "BUSCAR ZONA"], ["5.", "REGRESAR AL MENU PRINCIPAL"]]
+    print(tabulate(menu, tablefmt="grid"))
+    opcion = input("Seleccione una opcion : ")
+    if opcion not in opciones:
+        print('Ingresaste una opciones invalida')
+        os.system('pause')
+        menuZon()
+    else:
+        return opcion
+    
+def menuAsignacionA():
+    os.system('cls')
+    titulo4 = """
+        ++++++++++++++++++++++++++++++++
+        -  MENU ASIGNACION DE ACTIVOS  -
+        ++++++++++++++++++++++++++++++++
+        """
+    print(titulo4)
+    opciones = ["1","2","3"]
+    menu =[["1.", "CREAR ASIGNACION"], ["2.", "BUSCAR ASIGNACION"], ["3.", "REGRESAR AL MENU PRINCIPAL"]]
+    print(tabulate(menu, tablefmt="grid"))
+    opcion = input("Seleccione una opcion : ")
+    if opcion not in opciones:
+        print('Ingresaste una opciones invalida')
+        os.system('pause')
+        os.system('cls')
+        menuAsignacionA()
+    else:
+        return opcion
+    
+def menuReportes():
+    os.system('cls')
+    titulo4 = """
+        *+++++++++++++++++*
+        /  MENU REPORTES  /
+        *+++++++++++++++++*
+        """
+    print(titulo4)
+    opciones = ["1","2","3","4","5","6"]
+    menu =[["1.", "LISTAR TODOS LOS ACTIVOS"], ["2.", "LISTAR ACTIVOS POR CATEGORIA"], ["3.", "LISTAR ACTIVOS DADOS DE BAJA POR DAÑO"], ["4.", "LISTAR ACTIVOS Y ASIGNACION"], ["5.", "LISTAR HISTORIAL DE MOV. DE ACTIVO"], ["6.", "REGRESAR AL MENU PRINCIPAL"]]
+    print(tabulate(menu, tablefmt="grid"))
+    opcion = input("Seleccione una opcion : ")
+    if opcion not in opciones:
+        print('Ingresaste una opciones invalida')
+        os.system('pause')
+        os.system('cls')
+        menuReportes()
+    else:
+        return opcion
+    
+    
+    
+def menuMAct():
+    os.system('cls')
+    titulo4 = """
+        --------------------------------
+        /  MENU MOVIMIENTO DE ACTIVOS  /
+        --------------------------------
+        """
+    print(titulo4)
+    opciones = ["1","2","3","4","5"]
+    menu =[["1.", "RETORNO DE ACTIVO"], ["2.", "DAR DE BAJA ACTIVO"], ["3.", "CAMBIAR ASIGNACION DE ACTIVO"], ["4.", "ENVIAR A GARANTIA ACTIVO"], ["5.", "REGRESAR AL MENU PRINCIPAL"]]
+    print(tabulate(menu, tablefmt="grid"))
+    opcion = input("Seleccione una opcion : ")
+    if opcion not in opciones:
+        print('Ingresaste una opciones invalida')
+        os.system('pause')
+        os.system('cls')
+        menuMAct()
+    else:
+        return opcion
